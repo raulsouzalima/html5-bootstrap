8640b08e1f9812fdcd57b5ea4926b79146ab08b3
Raul Souza Lima <raulsouzalima@gmail.com>
2012-10-28 02:31:18 -0200

    Moving files from 'files' directory to root

--- main.rb
+++ main.rb
@@ -1,3 +1,9 @@
 puts "Deleting old git files and README"
 system "rm -Rf .gitignore .git README"
 puts "Done!"
+
+puts "\n\n"
+
+puts "Moving files from 'files' directory to root"
+system "mv files/* ."
+puts "Done!"
